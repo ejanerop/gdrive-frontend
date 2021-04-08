a70e5631d498fbb5f57723e0d062f8f605e9770e
Eric Janero <ejanerop97@gmail.com>
2021-04-08 13:59:37 -0400

    added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ListComponent } from './components/list/list.component';
 import { RedirectComponent } from './components/redirect/redirect.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path : 'login/:token' , component : RedirectComponent},
-  {path : 'list' , component : ListComponent},
+  {path : 'list' , component : ListComponent , canActivate : [AuthGuard]},
   {path : 'home' , component : HomeComponent},
   {path : '**' , pathMatch : 'full' , redirectTo : 'home'},
 ];
